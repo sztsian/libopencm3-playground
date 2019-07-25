b493cfeff47c0e1b0422e7ed7c16d124310c67d9
Zamir SUN <sztsian@gmail.com>
2019-07-25 21:10:32 +0800

    stm32f103: Improve 7-segment-display to show 1 to F in first display.

--- stm32f103/7-segment-display/7-segment-display.c
+++ stm32f103/7-segment-display/7-segment-display.c
@@ -1,6 +1,12 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
+__CHAR16_TYPE__ table[]={
+	0x3f, 0x06, 0x5b, 0x4f, 0x66,
+	0x6d, 0x7d, 0x07, 0x7f, 0x6f,
+	0x77, 0x7c, 0x39, 0x5e, 0x79,
+	0x71
+};
 static void gpio_setup(void)
 {
 	/* Enable GPIOC clock. */
@@ -33,15 +39,20 @@ int main(void)
 	// Select the segment
 	//gpio_set(GPIOB,GPIO6);
 	// Send segment data
-	// This is char E with GPIOB_6 (segment selection bit)
+	// This is char E in GPIOB_8 to GPIOB_15 with GPIOB_6 (segment selection bit, 0x40)
 	GPIOB_ODR = 0x7940;
 	//gpio_clear(GPIOB,GPIO6);
-
+    int digit=0;
 	while (1) {
+		digit = digit % 16;
+		GPIOB_ODR = ( table[digit]<<8 ) | 0x40;
+		digit = digit + 1;
 		for (i = 0; i < 800000; i++)	/* Wait a bit. */
 			__asm__("nop");
 		// Blink the dot on the display
 		gpio_toggle(GPIOB,GPIO15);
+		for (i = 0; i < 800000; i++)	/* Wait a bit. */
+			__asm__("nop");
 	}
 
 	return 0;
